95dcc03656455ea1a229fc8270756a07f0a4a502
seffka <seva@ringrows.ru>
2017-04-12 15:31:45 +0200

    Update tuning and duration in template file.

--- utils/prepareJSON.py
+++ utils/prepareJSON.py
@@ -5,6 +5,8 @@ import json
 import os
 import re
 import sys
+import numpy as np
+from essentia.streaming import *
 
 import acoustid
 import musicbrainzngs as m
@@ -39,11 +41,43 @@ def aidmatch(filename):
         sys.exit(1)
     return results
 
+def extractTuningAndDuration(infile):
+    chordHopSize = 2048
+    frameSize = 8192
+    loader = MonoLoader(filename=infile)
+    framecutter = FrameCutter(hopSize=chordHopSize, frameSize=frameSize)
+    windowing = Windowing(type="blackmanharris62")
+    spectrum = Spectrum()
+    spectralpeaks = SpectralPeaks(orderBy="frequency",
+                                  magnitudeThreshold=1e-05,
+                                  minFrequency=40,
+                                  maxFrequency=5000,
+                                  maxPeaks=10000)
+    tuning = TuningFrequency()
+    duration = Duration()
+    # use pool to store data
+    pool = essentia.Pool()
+    # connect algorithms together
+    loader.audio >> framecutter.signal
+    loader.audio >> duration.signal
+    framecutter.frame >> windowing.frame >> spectrum.frame
+    spectrum.spectrum >> spectralpeaks.spectrum
+    spectralpeaks.magnitudes >> tuning.magnitudes
+    spectralpeaks.frequencies >> tuning.frequencies
+    tuning.tuningFrequency >> (pool, 'tonal.tuningFrequency')
+    tuning.tuningCents >> (pool, 'tonal.tuningCents')
+    duration.duration >> (pool, 'duration.duration')
+    # network is ready, run it
+    print 'Processing audio file...', infile
+    essentia.run(loader)
+    return np.average(pool['tonal.tuningFrequency']), pool['duration.duration']
+
+
 ######################################################################
 
 inputDirs = [
     "/Users/seffka/Desktop/JazzHarmony/SmithsonianCollectionOfClassicJazz",
-    "/Users/seffka/Desktop/JazzHarmony/Jazz (The Smithsonian Anthology, 6CD) [320]"
+    "/Users/seffka/Desktop/JazzHarmony/Jazz_ The Smithsonian Anthology"
 ]
 
 outfile = "draft.json"
@@ -66,17 +100,18 @@ for inputDir in inputDirs:
             if re.search('(\.wav$)|(\.mp3$)|(\.flac$)', fname):
                 pathname = '/'.join((path, fname))
                 print pathname
+                tuning, duration = extractTuningAndDuration(pathname)
+                print tuning, duration
                 for score, rid, title, artist in aidmatch(pathname):
                     if (rid in recordingsSet and not rid in ids):
-                        # TODO duration & tuning !!!!!!!!!!!!!!!!!!!!!!!!!!
                         ids.add(rid)
                         entry = {}
                         entry['mbid'] = rid
                         entry['title'] = title
                         entry['artist'] = artist
-                        entry['tuning'] = 'TODO'
+                        entry['tuning'] =  round(tuning, 2)
                         entry['metre'] = '4/4'
-                        entry['duration'] = 'TODO'
+                        entry['duration'] = round(duration, 2)
                         pathname = pathname.replace(data_root, '$JAZZ_HARMONY_DATA_ROOT/')
                         entry['sandbox'] = {'path':pathname, 'transcriptions':[]}
                         res.append(entry)
