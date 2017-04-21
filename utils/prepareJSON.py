51ced6d382cf72eef7d48b1d0c756978ec853236
Emir Demirel <emir.demrel@gmail.com>
2017-04-21 15:58:14 +0200

    Modified prepareJSON

--- utils/prepareJSON.py
+++ utils/prepareJSON.py
@@ -5,6 +5,7 @@ import json
 import os
 import re
 import sys
+import argparse
 import numpy as np
 from essentia.streaming import *
 
@@ -14,6 +15,27 @@ import musicbrainzngs as m
 m.set_useragent("application", "0.01", "http://example.com")
 
 API_KEY = 'cSpUJKpD'
+
+def get_args():
+    # Description for documentation
+    parser = argparse.ArgumentParser(
+        description='Path to the audio files')
+  
+    parser.add_argument(
+        '-p', '--path', type=str, help='Path to Audio files', required=True)
+    parser.add_argument(
+        '-m', '--mid', type=str, help='MusicBrainzID', required=True)
+    parser.add_argument(
+        '-o', type=argparse.FileType('w'), default='draft.json', help='output file (default is: draft.json)')
+    # Array for all arguments passed to script
+    args = parser.parse_args()
+    # Assign args to variables
+    inputdir = args.path
+    musicbrainzid = args.mid
+    
+    
+    # Return all variable values
+    return inputdir, musicbrainzid, args.o.name
 
 def collectRecordingIds(d, result):
     if (type(d) is dict):
@@ -75,46 +97,39 @@ def extractTuningAndDuration(infile):
 
 ######################################################################
 
-inputDirs = [
-    "/Users/seffka/Desktop/JazzHarmony/SmithsonianCollectionOfClassicJazz",
-    "/Users/seffka/Desktop/JazzHarmony/Jazz_ The Smithsonian Anthology"
-]
-
-outfile = "draft.json"
+inputDir, musicbrainzid, outfile = get_args()
+print inputDir, musicbrainzid, outfile
 
 ids = set()
 
-# "inside" information about Smithsonian collection
 recordingsSet = set()
-release =  m.get_release_by_id("a90eeed6-dc07-4173-938c-49e63142f299", includes=['recordings'])
+release =  m.get_release_by_id(musicbrainzid, includes=['recordings'])
 collectRecordingIds(release, recordingsSet)
-release =  m.get_release_by_id("cfe54ee2-81b1-4942-b0f4-d977d8f15b29", includes=['recordings'])
-collectRecordingIds(release, recordingsSet)
+
 
 data_root = os.environ['JAZZ_HARMONY_DATA_ROOT']
 
 res = []
-for inputDir in inputDirs:
-    for path, dname, fnames in os.walk(inputDir):
-        for fname in fnames:
-            if re.search('(\.wav$)|(\.mp3$)|(\.flac$)', fname):
-                pathname = '/'.join((path, fname))
-                print pathname
-                tuning, duration = extractTuningAndDuration(pathname)
-                print tuning, duration
-                for score, rid, title, artist in aidmatch(pathname):
-                    if (rid in recordingsSet and not rid in ids):
-                        ids.add(rid)
-                        entry = {}
-                        entry['mbid'] = rid
-                        entry['title'] = title
-                        entry['artist'] = artist
-                        entry['tuning'] =  round(tuning, 2)
-                        entry['metre'] = '4/4'
-                        entry['duration'] = round(duration, 2)
-                        pathname = pathname.replace(data_root, '$JAZZ_HARMONY_DATA_ROOT/')
-                        entry['sandbox'] = {'path':pathname, 'transcriptions':[]}
-                        res.append(entry)
-                        break;
+for path, dname, fnames in os.walk(inputDir):
+    for fname in fnames:
+        if re.search('(\.wav$)|(\.mp3$)|(\.flac$)', fname):
+            pathname = '/'.join((path, fname))
+            print pathname
+            tuning, duration = extractTuningAndDuration(pathname)
+            print tuning, duration
+            for score, rid, title, artist in aidmatch(pathname):
+                if (rid in recordingsSet and not rid in ids):
+                    ids.add(rid)
+                    entry = {}
+                    entry['mbid'] = rid
+                    entry['title'] = title
+                    entry['artist'] = artist
+                    entry['tuning'] =  round(tuning, 2)
+                    entry['metre'] = '4/4'
+                    entry['duration'] = round(duration, 2)
+                    pathname = pathname.replace(data_root, '$JAZZ_HARMONY_DATA_ROOT/')
+                    entry['sandbox'] = {'path':pathname, 'transcriptions':[]}
+                    res.append(entry)
+                    break;
 
 json.dump(res, open(outfile,'w'), indent=True)
