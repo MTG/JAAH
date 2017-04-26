c39ff6009c199470b950fbf5afd36120b9938d3a
Emir Demirel <emir.demrel@gmail.com>
2017-04-26 15:03:13 +0200

    Modified json2Chord.py with argparse

--- utils/json2ChordLab.py
+++ utils/json2ChordLab.py
@@ -1,10 +1,27 @@
 import sys
 from chordUtils import json2lab
+import argparse
+
+def get_args():
+    parser = argparse.ArgumentParser(
+        description='convert input .json file to SonicVisualizer readable .lab file')
+    parser.add_argument(
+        '-i', '--input', type=str, help='input annotation file in .json file', required = True)
+    parser.add_argument(
+        '-o', '--output', type=str, default='output.lab', help='output file in .lab file (default output.lab)', required = True)
+    args = parser.parse_args()
+    infile = args.input
+    outfile = args.output 
+    
+    return infile, outfile
 
 try:
-    infile = sys.argv[1]
-    outfile = sys.argv[2]
+    infile, outfile = get_args()
+    
 except:
     print "usage:", sys.argv[0], "<input json file> <output lab file>"
     sys.exit()
+    
+ 
+    
 json2lab(infile, outfile)
